811568b8279342ef82dfb1f5b7426fe39caff8a0
Jenish <Jenish@JENISH>
2021-05-11 11:55:31 +0530

    Add project files.

---
+++ Project2.ObjectModel/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project2.ObjectModel
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+    }
+}
---
+++ Project2.ObjectModel/Project2DB.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Project2.ObjectModel
+{
+    public class Project2DB : DbContext
+    {
+        public Project2DB() 
+            : base("name=Project1DB")
+        {            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
